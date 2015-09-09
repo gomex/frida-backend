5b7bc19bf3bff2c7689fab29e575c8eed121d2f5
Rodrigo <rodrigovieira18@gmail.com>
2015-09-09 15:37:04 -0300

    Add new resource for insert new post

--- test/specs/contract/posts-specs.js
+++ test/specs/contract/posts-specs.js
@@ -6,26 +6,27 @@ var assert = require('assert'),
 ;
 
 describe('Posts:', function() {
-  var post,
+  var rawData,
       postId,
-      URL = '/api/organization/';
+      URL,
+      API = '/api/organization/';
 
   before(function(done){
     require(CONFIG.ROOT_DIRECTORY + '/lib/server').startServer();
 
-    post = {
-      name: 'Name',
-      path: 'Path',
-      sha: 'sha',
-      organization: 'organization-test',
-      repository: 'repository-test',
-      year: CONFIG.YEAR,
-      month: CONFIG.MONTH
+    rawData =  {
+      post: {
+        body: '',
+        metadata: {}
+      }
     };
 
-    URL += post.organization + '/' + post.repository + '/posts';
+    URL = {
+      get :  API + 'brasil-de-fato/site/posts',
+      insert :  API + 'brasil-de-fato/site/post'
+    };
 
-    postsRepository.insert(post, function(id) {
+    postsRepository.insert(rawData.post, function(id) {
       postId = id;
       done();
     });
@@ -37,9 +38,22 @@ describe('Posts:', function() {
     });
   });
 
-  it('GET: /api/organization/:organization/:repository/posts/:year/:month', function(done){
+  it('POST: /api/organization/:organization/:repository/post', function(done) {
+    api.post(URL.insert)
+        .send(rawData)
+        .expect(201)
+        .end(function(err, res) {
+          assert(typeof res.body.id !== 'undefined');
+          postsRepository.deleteById(res.body.id, function(err) {
+            done();
+          })
+        });
 
-    api.get(URL + '/' + CONFIG.YEAR + '/' + CONFIG.MONTH)
+  });
+
+  it('GET: /api/organization/:organization/:repository/posts?:filters', function(done){
+
+    api.get(URL.get + '?year=' + CONFIG.YEAR + '&month=' + CONFIG.MONTH)
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
@@ -51,12 +65,10 @@ describe('Posts:', function() {
         assert(res.body instanceof Array);
 
         res.body.forEach(function (response) {
-          assert.equal(typeof response.name !== 'undefined', true);
-          assert.equal(typeof response.path !== 'undefined', true);
-          assert.equal(typeof response.organization !== 'undefined', true);
-          assert.equal(typeof response.repository !== 'undefined', true);
-          assert.equal(typeof response.year !== 'undefined', true);
-          assert.equal(typeof response.month !== 'undefined', true);
+          assert.equal(typeof response._id !== 'undefined', true);
+          assert.equal(typeof response.insertDate !== 'undefined', true);
+          assert.equal(typeof response.metadata !== 'undefined', true);
+          assert.equal(typeof response.body !== 'undefined', true);
         });
 
         done();
@@ -65,7 +77,7 @@ describe('Posts:', function() {
 
   it('GET: /api/organization/:organization/:repository/posts', function(done){
 
-    api.get(URL)
+    api.get(URL.get)
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
@@ -77,9 +89,10 @@ describe('Posts:', function() {
         assert(res.body instanceof Array);
 
         res.body.forEach(function (response) {
-          assert.equal(typeof response.name !== 'undefined', true);
-          assert.equal(typeof response.path !== 'undefined', true);
-          assert.equal(typeof response.sha !== 'undefined', true);
+          assert.equal(typeof response._id !== 'undefined', true);
+          assert.equal(typeof response.insertDate !== 'undefined', true);
+          assert.equal(typeof response.metadata !== 'undefined', true);
+          assert.equal(typeof response.body !== 'undefined', true);
         });
 
         done();
