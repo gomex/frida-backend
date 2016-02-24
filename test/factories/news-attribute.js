ebaad8d59978f7791af868bafedc17369dbccbea
Michael Feitosa <mkaelzinho@gmail.com>
2016-02-24 18:01:00 -0300

    [Michael, Patrick] Add news attribute factory. Uses factories on news-util

---
+++ test/factories/news-attribute.js
@@ -0,0 +1,22 @@
+var Factory = require('rosie').Factory;
+var faker = require('faker');
+
+var metadata = new Factory()
+  .attr('title', function() { return faker.lorem.sentence() })
+  .attr('layout', 'post')
+  .attr('edition', 'minas-gerais')
+  .attr('area', 'nacional')
+  .attr('display_area', 'destaque_foto_grande')
+  .attr('hat', function() { return faker.lorem.words() })
+  .attr('description', function() { return faker.lorem.sentences() })
+  .attr('author', function() { return faker.name.findName() })
+  .attr('place', function() { return faker.address.state() })
+  .attr('cover', {})
+  .attr('files', []);
+
+var newsAttribute = new Factory()
+  .attr('body', function() { return faker.lorem.paragraphs() })
+  .attr('metadata', function() { return metadata.build() });
+
+module.exports.metadata = metadata;
+module.exports.newsAttribute = newsAttribute;
--- test/unit/lib/news/news-util.js
+++ test/unit/lib/news/news-util.js
@@ -2,8 +2,8 @@ var assert  = require('assert');
 var sinon   = require('sinon');
 
 var NewsUtil        = require('../../../../lib/news/news-util');
-var newsTestHelper  = require('../../../helpers/news');
-
+var newsAttributeFactory = require('../../../factories/news-attribute').newsAttribute;
+var metadataFactory = require('../../../factories/news-attribute').metadata;
 
 describe('news-util:', function() {
 
@@ -17,19 +17,9 @@ describe('news-util:', function() {
 
   after(function () { clock.restore(); });
 
-  describe('Prepare news to insert',function(){
-
-    it('should slugify url using title on metadata', function(done) {
-      var body = { metadata: {title: 'como vai'}  };
-      var expect = '2014/08/25/como-vai/';
-      var result = NewsUtil.prepare(body);
-
-      assert.equal(expect, result.metadata.url);
-      done();
-    });
-
+  describe('prepare news to insert',function(){
     it('sets created_at', function(done) {
-      var news = newsTestHelper.createNews();
+      var news = newsAttributeFactory.build();
       var expect = Date.now();
       var result = NewsUtil.prepare(news);
 
@@ -38,51 +28,35 @@ describe('news-util:', function() {
     });
   });
 
-  describe('format url',function(){
-    it('should be lower case', function(done) {
-      var body = { metadata: {title: 'Como Vai'}  };
-      var expect = '2014/08/25/como-vai/';
-      var result = NewsUtil.prepare(body);
+  describe('format url', function() {
+    it('slugifies url using title from metadata and created date', function(done) {
+      var metadata = metadataFactory.build({title: '"como" vai'});
+      var news = newsAttributeFactory.build({metadata: metadata});
+      var urlPath = '2014/08/25/como-vai/';
+      var result = NewsUtil.prepare(news);
 
-      assert.equal(expect, result.metadata.url);
+      assert.equal(urlPath, result.metadata.url);
       done();
     });
 
-    it('should have year, month and day of the news date on begining of url', function(done) {
-      var body = { metadata: {title: 'Como Vai'}  };
-      var expect = '2014/08/25/como-vai/';
-      var result = NewsUtil.prepare(body);
+    it('is lower case', function(done) {
+      var metadata = metadataFactory.build({title: 'CoMo Vai'});
+      var news = newsAttributeFactory.build({metadata: metadata});
+      var urlPath = '2014/08/25/como-vai/';
+      var result = NewsUtil.prepare(news);
 
-      assert.equal(expect, result.metadata.url);
+      assert.equal(urlPath, result.metadata.url);
       done();
     });
 
-    it('should replace [not-a-link] to a empty string', function(done) {
-      var body = { metadata: {edition: '[not-a-link]', title: '"Como" Vai'}  };
-      var expect = '2014/08/25/como-vai/';
-      var result = NewsUtil.prepare(body);
+    it('replaces [not-a-link] to a empty string', function(done) {
+      var metadata = metadataFactory.build({edition: '[not-a-link]', title: 'CoMo Vai'});
+      var news = newsAttributeFactory.build({metadata: metadata});
+      var urlPath = '2014/08/25/como-vai/';
+      var result = NewsUtil.prepare(news);
 
-      assert.equal(expect, result.metadata.url);
+      assert.equal(urlPath, result.metadata.url);
       done();
     });
-
-    it('should remove quotation marks', function(done) {
-      var body = { metadata: {title: '"Como" Vai'}  };
-      var expect = '2014/08/25/como-vai/';
-      var result = NewsUtil.prepare(body);
-
-      assert.equal(expect, result.metadata.url);
-      done();
-    });
-
-    it('should create url when title is empty', function(done) {
-      var body = { metadata: {title: ''} };
-      var result = NewsUtil.prepare(body);
-
-      assert.ok(!!result.metadata.url);
-      done();
-    });
-
   });
-
 });
