1f2d80e171894fc677409c66fe3f231d59149345
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-03-07 13:38:49 -0300

    [Patrick] - #335: adds cover and files attributes to the news created by news-attribute factory

--- test/factories/news-attribute.js
+++ test/factories/news-attribute.js
@@ -1,18 +1,28 @@
 var Factory = require('rosie').Factory;
 var faker = require('faker');
 
+var cover = new Factory()
+  .attr('link', function() { return faker.image.imageUrl(); })
+  .attr('thumbnail', function() { return faker.image.imageUrl(); })
+  .attr('medium', function() { return faker.image.imageUrl(); })
+  .attr('small', function() { return faker.image.imageUrl(); })
+  .attr('title', 'The picture\'s title')
+  .attr('cover', true)
+  .attr('credits', 'Photographer')
+  .attr('subtitle', 'A beautiful picture');
+
 var metadata = new Factory()
   .attr('title', function() { return faker.lorem.sentence(); })
   .attr('layout', 'post')
   .attr('edition', 'minas-gerais')
   .attr('area', 'nacional')
   .attr('display_area', 'destaque_foto_grande')
-  .attr('hat', function() { return faker.lorem.words(); })
+  .attr('hat', 'Olimpíadas')
   .attr('description', function() { return faker.lorem.sentences(); })
   .attr('author', function() { return faker.name.findName(); })
   .attr('place', function() { return faker.address.state(); })
-  .attr('cover', {})
-  .attr('files', []);
+  .attr('cover', function() { return cover.build(); })
+  .attr('files', function() { return [cover.build() ]; });
 
 var newsAttribute = new Factory()
   .attr('body', function() { return faker.lorem.paragraphs(); })
