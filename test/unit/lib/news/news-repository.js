110ca2b5e8a90767018f8b372833fae78492d724
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-03-09 14:31:35 -0300

    [Patrick] - #335: adds find function to news repository

--- test/unit/lib/news/news-repository.js
+++ test/unit/lib/news/news-repository.js
@@ -114,4 +114,84 @@ describe('news-repository', function(){
       });
     });
   });
+
+  describe('find', function(){
+    it('finds news matching some criteria', function(done){
+      var metadata1 = metadataFactory.build({display_area: 'featured_1'});
+      var news1 = newsFactory.build({metadata: metadata1});
+      var metadata2 = metadataFactory.build({display_area: 'featured_2'});
+      var news2 = newsFactory.build({metadata: metadata2});
+
+      async.parallel([
+        async.apply(newsRepository.insert, news1),
+        async.apply(newsRepository.insert, news2)
+      ], function(err, insertedIds){
+        var criteria = {'metadata.display_area': 'featured_1'};
+        newsRepository.find(criteria, null, null, null, function(err, result){
+          assert.equal(result[0]._id, insertedIds[0]);
+          done();
+        });
+      });
+    });
+
+    it('returns only the specified fields in the result set', function(done){
+      var metadata1 = metadataFactory.build({display_area: 'featured_1'});
+      var news1 = newsFactory.build({metadata: metadata1});
+      var metadata2 = metadataFactory.build({display_area: 'featured_2'});
+      var news2 = newsFactory.build({metadata: metadata2});
+
+      async.parallel([
+        async.apply(newsRepository.insert, news1),
+        async.apply(newsRepository.insert, news2)
+      ], function(err, _insertedIds){
+        var query = {'metadata.display_area': 'featured_1'};
+        var projection = {
+          '_id': false,
+          'title': '$metadata.title'
+        };
+        newsRepository.find(query, projection, null, null, function(err, result){
+          assert.equal(result[0]._id, undefined);
+          assert.equal(result[0].metadata, undefined);
+          assert.equal(result[0].body, undefined);
+          assert.equal(result[0].title, news1.metadata.title);
+          done();
+        });
+      });
+    });
+
+    it('limits the result set to the specified amount', function(done){
+      var news1 = newsFactory.build();
+      var news2 = newsFactory.build();
+
+      async.parallel([
+        async.apply(newsRepository.insert, news1),
+        async.apply(newsRepository.insert, news2)
+      ], function(err, _insertedIds){
+        newsRepository.find({}, null, 1, null, function(err, result){
+          assert.equal(result.length, 1);
+          done();
+        });
+      });
+    });
+
+    it('sorts the result set using some field', function(done){
+      var metadata1 = metadataFactory.build({title: 'a'});
+      var news1 = newsFactory.build({metadata: metadata1});
+      var metadata2 = metadataFactory.build({title: 'b'});
+      var news2 = newsFactory.build({metadata: metadata2});
+
+      async.parallel([
+        async.apply(newsRepository.insert, news1),
+        async.apply(newsRepository.insert, news2)
+      ], function(err, _insertedIds){
+        newsRepository.find({}, null, null, { 'metadata.title': -1 }, function(err, result){
+          assert.equal(result[0].metadata.title, 'b');
+          assert.equal(result[1].metadata.title, 'a');
+          done();
+        });
+      });
+    });
+
+  });
+
 });
