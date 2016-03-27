bbd3f68670b68d3c0e2dbf0784b4c38220609f08
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-03-27 11:40:19 -0300

    [Patrick] - #381: generates data file for area home page on news publishing

--- test/unit/lib/publisher/hexo.js
+++ test/unit/lib/publisher/hexo.js
@@ -1,3 +1,4 @@
+var async   = require('async');
 var assert  = require('assert');
 var fs      = require('fs');
 var grayMatter =  require('gray-matter');
@@ -5,7 +6,11 @@ var moment  = require('moment');
 var slug =   require('slug');
 var _ = require('underscore');
 
-var hexo = require('../../../../lib/publisher/hexo');
+var metadataFactory = require('../../../factories/news-attribute').metadata;
+var newsFactory     = require('../../../factories/news-attribute').newsAttribute;
+
+var hexo            = require('../../../../lib/publisher/hexo');
+var newsRepository  = require('../../../../lib/news/news-repository');
 
 describe('hexo', function() {
 
@@ -84,6 +89,75 @@ describe('hexo', function() {
       }, TypeError);
 
       done();
+    });
+
+    describe('creates area data file', function(){
+      var lastNews = [];
+
+      before(function(done) {
+        var insertNewsOperations = [];
+
+        var metadata = metadataFactory.build({ url: '2016/03/news-' + Date.now() });
+        var news = newsFactory.build({ metadata: metadata, published_at: new Date(), status: 'published' });
+        for(var i = 0; i < 20; i++) {
+          insertNewsOperations.push(async.apply(newsRepository.insert, news));
+          lastNews.push(news);
+        }
+
+        async.parallel(insertNewsOperations, function(err) {
+          if(err) throw err;
+          done();
+        });
+      });
+
+      it('in its correspondent hexo source folder', function(done) {
+        var news = _.last(lastNews);
+        var expectedPath = process.env.HEXO_SOURCE_PATH + '/' + news.metadata.area  + '/index.md';
+
+        try { fs.unlinkSync(expectedPath); } catch(e) { /* make sure the file was not there before test execution */ }
+
+        hexo.publish(news, function(err) {
+          assert.equal(null, err);
+          assert.ok(fs.existsSync(expectedPath));
+
+          done();
+        });
+      });
+
+      // se a noticia for do tipo coluna ela deve estar em colunas
+
+      it('with layout "news-list" and a simplified version of the last 20 published news for the area', function(done) {
+        var news = _.last(lastNews);
+
+        hexo.publish(news, function(err) {
+          if(err) throw err;
+
+          var simplifiedNews = _.map(lastNews, function(item) {
+            return {
+              cover: {
+                url: item.metadata.cover.link,
+                small: item.metadata.cover.small,
+                credits: item.metadata.cover.credits,
+                subtitle: item.metadata.cover.subtitle
+              },
+              hat: item.metadata.hat,
+              title: item.metadata.title,
+              description: item.metadata.description,
+              path: item.metadata.url,
+              date: item.published_at
+            };
+          });
+
+          var areaIndexFilePath = process.env.HEXO_SOURCE_PATH + '/' + news.metadata.area  + '/index.md';
+          var areaIndexFile   = fs.readFileSync(areaIndexFilePath, 'utf-8');
+          var areaIndexObject = grayMatter(areaIndexFile);
+
+          assert.equal(areaIndexObject.data.layout, 'news-list');
+          assert.deepEqual(areaIndexObject.data[news.metadata.area], simplifiedNews);
+
+          done();
+        });
+      });
     });
   });
 
