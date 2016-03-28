d41d2c806392e843c0a623c8be2be4c1648badf6
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-03-28 16:15:04 -0300

    [Patrick] - #381: area-page-strategy is also capable of creating data for columns page

--- test/unit/lib/publisher/area-page-strategy.js
+++ test/unit/lib/publisher/area-page-strategy.js
@@ -4,60 +4,109 @@ var _           = require('underscore');
 var async       = require('async');
 var assert      = require('assert');
 
-var metadataFactory = require('../../../factories/news-attribute').metadata;
-var newsFactory     = require('../../../factories/news-attribute').newsAttribute;
+var metadataFactory         = require('../../../factories/news-attribute').metadata;
+var newsFactory             = require('../../../factories/news-attribute').newsAttribute;
+var columnMetadataFactory   = require('../../../factories/column-attributes').columnMetadata;
+var columnFactory           = require('../../../factories/column-attributes').columnAttributes;
 
 var areaPageStrategy    = require('../../../../lib/publisher/area-page-strategy');
 var newsRepository      = require('../../../../lib/news/news-repository');
 
 describe('area-page-strategy', function() {
+
   describe('buildPageData', function() {
-    var lastNews = [];
 
-    before(function(done) {
-      var insertNewsOperations = [];
+    describe('when area is not column', function() {
+      var lastNews = [];
 
-      var metadata = metadataFactory.build({ url: '2016/03/news-' + Date.now() });
-      var news = newsFactory.build({ metadata: metadata, published_at: new Date(), status: 'published' });
-      for(var i = 0; i < 20; i++) {
-        insertNewsOperations.push(async.apply(newsRepository.insert, news));
-        lastNews.push(news);
-      }
+      before(function(done) {
+        var insertNewsOperations = [];
 
-      async.parallel(insertNewsOperations, function(err) {
-        if(err) throw err;
-        done();
+        var metadata = metadataFactory.build({ url: '2016/03/news-' + Date.now() });
+        var news = newsFactory.build({ metadata: metadata, published_at: new Date(), status: 'published' });
+        for(var i = 0; i < 20; i++) {
+          insertNewsOperations.push(async.apply(newsRepository.insert, news));
+          lastNews.push(news);
+        }
+
+        async.parallel(insertNewsOperations, function(err) {
+          if(err) throw err;
+          done();
+        });
+      });
+
+      it('area page data has layout "news_list" and a simplified version of the last 20 published news for the area', function(done) {
+        var news = _.last(lastNews);
+
+        areaPageStrategy.buildPageData(news.metadata.area, function(err, areaPageData) {
+          if(err) throw err;
+
+          var simplifiedNews = _.map(lastNews, function(item) {
+            return {
+              cover: {
+                url: item.metadata.cover.link,
+                small: item.metadata.cover.small,
+                credits: item.metadata.cover.credits,
+                subtitle: item.metadata.cover.subtitle
+              },
+              hat: item.metadata.hat,
+              title: item.metadata.title,
+              description: item.metadata.description,
+              path: item.metadata.url,
+              date: item.published_at
+            };
+          });
+
+          assert.equal(areaPageData.layout, 'news_list');
+          assert.equal(areaPageData.area, news.metadata.area);
+          assert.deepEqual(areaPageData.news, simplifiedNews);
+
+          done();
+        });
       });
     });
 
-    it('area page data has layout "news_list" and a simplified version of the last 20 published news for the area', function(done) {
-      var news = _.last(lastNews);
+    describe('when area is column', function() {
+      var lastColumns = [];
 
-      areaPageStrategy.buildPageData(news.metadata.area, function(err, areaPageData) {
-        if(err) throw err;
+      before(function(done) {
+        var insertColumnsOperations = [];
 
-        var simplifiedNews = _.map(lastNews, function(item) {
-          return {
-            cover: {
-              url: item.metadata.cover.link,
-              small: item.metadata.cover.small,
-              credits: item.metadata.cover.credits,
-              subtitle: item.metadata.cover.subtitle
-            },
-            hat: item.metadata.hat,
-            title: item.metadata.title,
-            description: item.metadata.description,
-            path: item.metadata.url,
-            date: item.published_at
-          };
+        var columnMetadata = columnMetadataFactory.build({ url: '2016/03/column-' + Date.now() });
+        var column = columnFactory.build({ metadata: columnMetadata, published_at: new Date(), status: 'published' });
+        for(var i = 0; i < 20; i++) {
+          insertColumnsOperations.push(async.apply(newsRepository.insert, column));
+          lastColumns.push(column);
+        }
+
+        async.parallel(insertColumnsOperations, function(err) {
+          if(err) throw err;
+          done();
         });
-
-        assert.equal(areaPageData.layout, 'news_list');
-        assert.equal(areaPageData.area, news.metadata.area);
-        assert.deepEqual(areaPageData.news, simplifiedNews);
-
-        done();
       });
+
+      it('page data has layout "columnists" and a simplified version of the last 20 published columns', function(done) {
+
+        areaPageStrategy.buildPageData('column', function(err, columnPageData) {
+          if(err) throw err;
+
+          var simplifiedColumns = _.map(lastColumns, function(item) {
+            return {
+              columnist: item.metadata.columnist,
+              title: item.metadata.title,
+              description: item.metadata.description,
+              path: item.metadata.url,
+              date: item.published_at
+            };
+          });
+
+          assert.equal(columnPageData.layout, 'columnists');
+          assert.deepEqual(columnPageData.columns, simplifiedColumns);
+
+          done();
+        });
+      });
+
     });
   });
 });
