d8a9099adad8e83b9a9459c16101a36a11cb005d
rgusmao <rgusmao@thoughtworks.com>
2016-05-13 15:18:54 -0300

    [Patrick, Renata] - NA: Sets work dir before run commands

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 PATH=$PATH:/usr/local/node/node-default/bin
-pm2 stop frida-backend
-git pull
-npm install
-pm2 start pm2-config.json
+
+pushd /home/frida/components/frida-backend
+  pm2 stop frida-backend
+  git pull
+  npm install
+  pm2 start pm2-config.json
+popd
