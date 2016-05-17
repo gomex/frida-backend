43534766637ef095f4793b3b1a5ba86c77dad857
Michael Feitosa <mkaelzinho@gmail.com>
2016-05-17 17:08:22 -0300

    [Michael] #443 - Unpublish regenerates home and areas.

--- test/unit/lib/news/publisher.js
+++ test/unit/lib/news/publisher.js
@@ -7,6 +7,7 @@ var sinonChai = require('sinon-chai');
 
 var publisher = require('../../../../lib/news/publisher');
 var repository = require('../../../../lib/news/news-repository');
+var hexo = require('../../../../lib/publisher/hexo');
 
 chai.use(sinonChai);
 
@@ -14,17 +15,33 @@ describe('publisher', function() {
   describe('.unpublish', function() {
     var subject = function(callback) { return publisher.unpublish(news, callback); };
 
-    var news = {id: '123', status: 'published'};
-    var updatedNews = {id: news.id, status: 'draft'};
+    var news = {
+      _id: '123',
+      status: 'published',
+      metadata: {
+        layout: 'post',
+        area: 'area'
+      }
+    };
+
+    var updatedNews = {
+      _id: news._id,
+      status: 'draft',
+      metadata: news.metadata
+    };
 
     beforeEach(function() {
-      sinon.stub(repository, 'updateById', function(id, news, callback) {
-        callback(null, updatedNews);
-      });
+      sinon.stub(repository, 'updateById').yields(null, updatedNews);
+      sinon.stub(hexo, 'unpublish').yields(null);
+      sinon.stub(hexo, 'updateAreaPage').yields(null);
+      sinon.stub(hexo, 'updateHomePage').yields(null);
     });
 
     afterEach(function() {
       repository.updateById.restore();
+      hexo.unpublish.restore();
+      hexo.updateAreaPage.restore();
+      hexo.updateHomePage.restore();
     });
 
     it('exists', function() {
@@ -33,17 +50,41 @@ describe('publisher', function() {
 
     it('returns updated news', function(done) {
       subject(function(err, news) {
-        done(err);
-
         expect(news.status).to.equal('draft');
+
+        done(err);
       });
     });
 
     it('saves changes', function(done) {
       subject(function(err, news) {
-        done(err);
+        expect(repository.updateById).to.have.been.calledWith(news._id, news);
 
-        expect(repository.updateById).to.have.been.calledWith(news.id, news);
+        done(err);
+      });
+    });
+
+    it('delegates to hexo', function(done) {
+      subject(function(err, news) {
+        expect(hexo.unpublish).to.have.been.calledWith(news);
+
+        done(err);
+      });
+    });
+
+    it('updates area', function(done) {
+      subject(function(err, news) {
+        expect(hexo.updateAreaPage).to.have.been.calledWith(news.metadata.area);
+
+        done(err);
+      });
+    });
+
+    it('updates home page', function(done) {
+      subject(function(err, _news) {
+        expect(hexo.updateHomePage).to.have.been.called;
+
+        done(err);
       });
     });
   });
