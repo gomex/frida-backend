0cc5a34a40151d86a977c46ac5c43504a8e68444
wandecleya <wandecleya@gmail.com>
2016-05-19 15:39:04 -0300

    [Patrick, Wandy] - #485: does not republish unchanged news

--- test/unit/lib/news/publisher.js
+++ test/unit/lib/news/publisher.js
@@ -8,12 +8,78 @@ var sinonChai = require('sinon-chai');
 var publisher = require('../../../../lib/news/publisher');
 var repository = require('../../../../lib/news/news-repository');
 var hexo = require('../../../../lib/publisher/hexo');
+var newsFactory = require('../../../factories/news-attribute').newsAttribute;
+var metadataFactory = require('../../../factories/news-attribute').metadata;
 
 chai.use(sinonChai);
 
 describe('publisher', function() {
+
+  describe('.publish', function() {
+    var subject = function(news, callback) { publisher.publish(news, callback); };
+
+    describe('when news is already published', function() {
+      beforeEach(function() {
+        sinon.stub(repository, 'updateById').yields(null, null);
+        sinon.stub(hexo, 'publish').yields(null);
+        sinon.stub(hexo, 'updateAreaPage').yields(null);
+        sinon.stub(hexo, 'updateHomePage').yields(null);
+      });
+
+      afterEach(function() {
+        repository.updateById.restore();
+        hexo.publish.restore();
+        hexo.updateAreaPage.restore();
+        hexo.updateHomePage.restore();
+      });
+
+      describe('and was not modified', function() {
+        var metadata = metadataFactory.build();
+        var news = newsFactory.build(
+          {
+            metadata: metadata,
+            published_at: new Date(),
+            updated_at: new Date(1000)
+          });
+
+        it('does not update status on database', function(done){
+          subject(news, function(err) {
+            expect(repository.updateById).to.not.have.been.called;
+
+            done(err);
+          });
+        });
+
+        it('does not create news data file', function(done){
+          subject(news, function(err) {
+            expect(hexo.publish).to.not.have.been.called;
+
+            done(err);
+          });
+        });
+
+        it('does not update area data file', function(done){
+          subject(news, function(err) {
+            expect(hexo.updateAreaPage).to.not.have.been.called;
+
+            done(err);
+          });
+        });
+
+        it('does not update home data file', function(done){
+          subject(news, function(err) {
+            expect(hexo.updateHomePage).to.not.have.been.called;
+
+            done(err);
+          });
+        });
+      });
+    });
+
+  });
+
   describe('.unpublish', function() {
-    var subject = function(callback) { return publisher.unpublish(news, callback); };
+    var subject = function(callback) { publisher.unpublish(news, callback); };
 
     var news = {
       _id: '123',
