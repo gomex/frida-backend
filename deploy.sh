16e9c0c00ff43a3ae9d55330d59edd78d87e263b
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-05-23 15:17:27 -0300

    [Patrick] - NA: accepts specific commit to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 PATH=$PATH:/usr/local/node/node-default/bin
+GIT_COMMIT_SHA=$1
 
 pushd /home/frida/components/frida-backend
   pm2 stop frida-backend
-  git pull
+  git fetch
+  git checkout ${GIT_COMMIT_SHA=master}
   npm install
   pm2 start pm2-config.json
 popd
