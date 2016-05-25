5688769024db0b6eefc065ad3e98851eddba81a2
Michael Feitosa <mkaelzinho@gmail.com>
2016-05-25 14:24:11 -0300

    [Michael] 1o5egkS8 Add tabloid model

---
+++ test/unit/lib/news/tabloid.js
@@ -0,0 +1,38 @@
+var tabloid = require('../../../../lib/news/tabloid');
+var repository = require('../../../../lib/news/news-repository');
+
+describe('tabloid', () => {
+  var subject = (callback) => tabloid.findNews(aTabloid, callback);
+
+  var aTabloid = { _id: '123' };
+  var criteria = {
+    '_id': aTabloid._id
+  };
+  var expectedNews = [];
+
+  beforeEach(() => {
+    sinon.stub(repository, 'find').yields(null, expectedNews);
+  });
+
+  afterEach(() => {
+    repository.find.restore();
+  });
+
+  it('searches news', (done) => {
+    subject((err) => {
+      expect(repository.find).to.have.been.calledWith(
+        criteria
+      );
+
+      done(err);
+    });
+  });
+
+  it('returns news', (done) => {
+    subject((err, news) => {
+      expect(news).to.equal(news);
+
+      done(err);
+    });
+  });
+});
