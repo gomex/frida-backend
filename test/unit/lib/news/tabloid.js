0bc5219225da212e17ca84d9622743c544ddcb97
Michael Feitosa <mkaelzinho@gmail.com>
2016-06-07 13:39:59 -0300

    [Michael] 1o5egkS8 Add find tabloid to news repository

--- test/unit/lib/news/tabloid.js
+++ test/unit/lib/news/tabloid.js
@@ -1,36 +1,74 @@
 var tabloid = require('../../../../lib/news/tabloids');
 var repository = require('../../../../lib/news/news-repository');
 var factory = require('../../../factories/tabloid-attributes').tabloid;
+var tabloidNewsFactory = require('../../../factories/tabloid-news-attributes').tabloid;
+
 
 describe('tabloid', () => {
-  var subject = (callback) => tabloid.findNews(aTabloid, callback);
+  describe('.findNews', () => {
+    var subject = (callback) => tabloid.findNews(aTabloid, callback);
 
-  var aTabloid = factory.build();
-  var criteria = {
-    'metadata.layout': 'tabloid_news',
-    status: 'published',
-    region: aTabloid.metadata.display_area,
-    edition: aTabloid.edition
-  };
-  var expectedNews = [];
+    var aTabloid = factory.build();
+    var criteria = {
+      'metadata.layout': 'tabloid_news',
+      status: 'published',
+      region: aTabloid.metadata.display_area,
+      edition: aTabloid.edition
+    };
+    var expectedNews = [];
 
-  beforeEach(() => {
-    sandbox.stub(repository, 'getAll').yields(null, expectedNews);
-  });
+    beforeEach(() => {
+      sandbox.stub(repository, 'getAll').yields(null, expectedNews);
+    });
 
-  it('searches news', (done) => {
-    subject((err) => {
-      expect(repository.getAll).to.have.been.calledWith(criteria);
+    it('searches news', (done) => {
+      subject((err) => {
+        expect(repository.getAll).to.have.been.calledWith(criteria);
 
-      done(err);
+        done(err);
+      });
+    });
+
+    it('returns news', (done) => {
+      subject((err, news) => {
+        expect(news).to.equal(expectedNews);
+
+        done(err);
+      });
     });
   });
 
-  it('returns news', (done) => {
-    subject((err, news) => {
-      expect(news).to.equal(news);
+  describe('.findTabloid', () => {
+    var subject = (callback) => tabloid.findTabloid(aNews, callback);
 
-      done(err);
+    var aNews = tabloidNewsFactory.build();
+    var criteria = {
+      'metadata.layout': 'tabloid',
+      status: 'published',
+      'metadata.display_area': aNews.region,
+      edition: aNews.edition
+    };
+
+    var expectedTabloid = {};
+
+    beforeEach(() => {
+      sandbox.stub(repository, 'find').yields(null, [expectedTabloid]);
+    });
+
+    it('searches news', (done) => {
+      subject((err) => {
+        expect(repository.find).to.have.been.calledWith(criteria, null, 1);
+
+        done(err);
+      });
+    });
+
+    it('returns news', (done) => {
+      subject((err, aTabloid) => {
+        expect(aTabloid).to.equal(expectedTabloid);
+
+        done(err);
+      });
     });
   });
 });
