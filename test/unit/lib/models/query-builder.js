44ae9c62febc2f005e09bf1a82325b2f91794e62
Michael Feitosa <mkaelzinho@gmail.com>
2016-07-14 12:47:02 -0300

    [Michael] GpyhX4Q2 Add filter by status

---
+++ test/unit/lib/models/query-builder.js
@@ -0,0 +1,67 @@
+/*eslint no-undef: "off"*/
+
+var queryBuilder = require('../../../../lib/models/news/query-builder');
+
+describe('queryBuilder', () => {
+  describe('.build', () => {
+    subj('build', () => queryBuilder.build(params));
+
+    given('params', () => ({}));
+
+    it('exists', () => {
+      expect(queryBuilder.build).to.exist;
+    });
+
+    it('returns empty filter', () => {
+      expect(build).to.eql({});
+    });
+
+    describe('with query', () => {
+      given('params', () => ({
+        q: 'query'
+      }));
+
+      given('query', () => ({
+        'metadata.title': new RegExp(params.q, 'i')
+      }));
+
+      it('returns', () => {
+        expect(build).to.eql(query);
+      });
+    });
+
+    describe('with status', () => {
+      given('params', () => ({
+        status: 'draft'
+      }));
+
+      given('query', () => ({
+        status: 'draft'
+      }));
+
+      it('returns', () => {
+        expect(build).to.eql(query);
+      });
+    });
+
+    describe('when has more status', () => {
+      given('params', () => ({
+        status: [
+          'draft',
+          'published'
+        ]
+      }));
+
+      given('query', () => ({
+        $or: [
+          {'status': 'draft'},
+          {'status': 'published'}
+        ]
+      }));
+
+      it('returns', () => {
+        expect(build).to.eql(query);
+      });
+    });
+  });
+});
