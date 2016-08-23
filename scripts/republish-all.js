5709352e4296894e41decde4fbaeb1b3cc1529e6
Michael Feitosa <mkaelzinho@gmail.com>
2016-08-23 16:13:50 -0300

    [Michael] XJkTlS22 Republish all news

--- scripts/republish-all.js
+++ scripts/republish-all.js
@@ -5,42 +5,18 @@ var News = require('../lib/models/news');
 var hexo = require('../lib/publisher/hexo');
 
 var publish = (news, cb) => {
-  if(news.status != 'published') {
-    return cb();
-  }
-  hexo.publish(news, cb);
+  console.log('republishing [%s]- "%s"', news.metadata.area, news.metadata.title);
+
+  async.series([
+    sleep,
+    async.apply(hexo.publish, news),
+  ], cb);
 };
 
 var sleep = (cb) => {
   setTimeout(() => {
     cb();
   }, 100);
-};
-
-var update = (news, cb) => {
-  console.log('republishing [%s]- "%s"', news.metadata.area, news.metadata.title);
-
-  async.series([
-    sleep,
-    news.save,
-    async.apply(publish, news)
-  ], cb);
-};
-
-var publishAreas = (areasToUpdate) => {
-
-  function publishAreaPage(area, callback) {
-    hexo.updateAreaPage(area, (err) => {
-      if(err) throw err;
-      console.log('published - %s', area);
-      callback();
-    });
-  }
-
-  async.eachSeries(areasToUpdate, publishAreaPage, (err) => {
-    if(err) throw err;
-    process.exit();
-  });
 };
 
 News.find({
@@ -50,18 +26,10 @@ News.find({
   if(err) throw err;
 
   console.log('%s news articles found', result.length);
-  var areas = {};
 
-  var createAreaList = (item, cb) => {
-    areas[item.metadata.area] = item.metadata.area;
-    cb(null);
-  };
-
-  async.map(result, createAreaList, (err) => {
+  async.eachSeries(result, publish, (err) => {
     if(err) throw err;
-    async.eachSeries(result, update, (err) => {
-      if(err) throw err;
-      publishAreas(Object.keys(areas));
-    });
+
+    process.exit();
   });
 });
