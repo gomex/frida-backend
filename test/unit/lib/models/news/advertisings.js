59dd790dfa6b49b932e03d5fb1bd8850a5c8f2dc
Michael Feitosa <mkaelzinho@gmail.com>
2016-09-27 16:57:05 -0300

    [kael] NA Add advertisings presenter

---
+++ test/unit/lib/models/news/advertisings.js
@@ -0,0 +1,69 @@
+/*eslint no-undef: "off"*/
+
+var News = require('../../../../../lib/models/news');
+var advertisings = require('../../../../../lib/models/news/advertisings');
+var factory = require('../../../../factories/advertising-attributes').advertising;
+
+describe('lib/models/news/t.js', () => {
+  describe('.getList', () => {
+    var subject = (callback) => advertisings.getList(callback);
+
+    given('criteria', () => ({
+      'metadata.layout': 'advertising',
+      status: 'published'
+    }));
+
+    given('advertising', () => factory.build());
+
+    beforeEach(() => {
+      sandbox.stub(News, 'find').yields(null, [advertising]);
+    });
+
+    it('exists', () => {
+      expect(advertisings.getList).to.exist;
+    });
+
+    it('searchs t', (done) => {
+      subject((err) => {
+        expect(News.find).to.have.been.calledWith(Object.assign(
+          criteria, {display_area: 'advertising_01'}
+        ));
+
+        expect(News.find).to.have.been.calledWith(Object.assign(
+          criteria, {display_area: 'advertising_02'}
+        ));
+
+        expect(News.find).to.have.been.calledWith(Object.assign(
+          criteria, {display_area: 'advertising_03'}
+        ));
+
+        expect(News.find).to.have.been.calledWith(Object.assign(
+          criteria, {display_area: 'advertising_04'}
+        ));
+
+        expect(News.find).to.have.been.calledWith(Object.assign(
+          criteria, {display_area: 'advertising_05'}
+        ));
+
+        expect(News.find).to.have.been.calledWith(Object.assign(
+          criteria, {display_area: 'advertising_06'}
+        ));
+
+        done(err);
+      });
+    });
+
+    it('returns list', () => {
+      subject((err, list) => {
+        expect(list).to.eql([
+          advertising,
+          advertising,
+          advertising,
+          advertising,
+          advertising,
+          advertising
+        ]);
+      });
+    });
+  });
+});
