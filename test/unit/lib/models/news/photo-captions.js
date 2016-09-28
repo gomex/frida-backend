73a3e9ee4af0be13bb54bc625ac30bcf6998aca0
rgusmao <rgusmao@thoughtworks.com>
2016-09-28 15:45:24 -0300

    [Kael, Renata] m5FcYh10 Photo caption has a new method to get related photo captions

--- test/unit/lib/models/news/photo-captions.js
+++ test/unit/lib/models/news/photo-captions.js
@@ -1,5 +1,8 @@
+/*eslint no-undef: "off"*/
+
 var photoCaption = require('../../../../../lib/models/news/photo-captions');
 var News = require('../../../../../lib/models/news');
+var factory = require('../../../../factories/photo-caption-attributes').photoCaption;
 
 describe('photoCaption', () => {
   describe('.getList', () => {
@@ -28,6 +31,70 @@ describe('photoCaption', () => {
         expect(news).to.equal(expectedNews);
 
         done(err);
+      });
+    });
+  });
+
+  describe('.getRelateds', () => {
+    var subject = (callback) => photoCaption.getRelateds(news, callback);
+
+    given('query', () => ({
+      'metadata.layout': 'photo_caption',
+      status: 'published'
+    }));
+
+    given('options', () => ({
+      limit: 3,
+      sort: '-created_at'
+    }));
+
+    given('news', () => new News(factory.build()));
+
+    given('newsList', () => (['foo', 'bar']));
+    given('expectedList', () => newsList);
+
+    beforeEach(() => {
+      sandbox.stub(News, 'find').yields(null, newsList);
+    });
+
+    it('exists', () => {
+      expect(photoCaption.getRelateds).to.exist;
+    });
+
+    it('find news', (done) => {
+      subject((err) => {
+        expect(News.find).to.have.been.calledWith(query, null, options);
+
+        done(err);
+      });
+    });
+
+    it('returns list', (done) => {
+      subject((err, list) => {
+        expect(list).to.deep.equal(expectedList);
+
+        done(err);
+      });
+    });
+
+    describe('when news is one of related news', () => {
+      given('otherNews1', () => new News(factory.build()));
+      given('otherNews2', () => new News(factory.build()));
+
+      given('newsList', () => ([news, otherNews1, otherNews2]));
+      given('expectedList', () => ([otherNews1, otherNews2]));
+
+      beforeEach(() => {
+        News.find.restore();
+        sandbox.stub(News, 'find').yields(null, newsList);
+      });
+
+      it('excludes news from relateds', (done) => {
+        subject((err, list) => {
+          expect(list).to.deep.equal(expectedList);
+
+          done(err);
+        });
       });
     });
   });
