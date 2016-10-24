ac1cf63ea1afa535a82ab63b07eae6094b567ace
Michael Feitosa <mkaelzinho@gmail.com>
2016-10-24 14:25:59 -0200

    [kael] wOOtbNlQ Creates bdf home when initializes

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ require('dotenv').config();
 require('./lib/db/initializer');
 require('./lib/columnist/columnist-repository').write(),
 require('./lib/http/server').startServer();
+
+var Home = require('./lib/models/home');
+
+Home.init((err) => {
+  if (err) {
+    return console.error('Error Home.init', err);
+  }
+});
