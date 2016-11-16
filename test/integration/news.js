554b9d92a16030f397c7ce5732791bf47bb24c19
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-11-16 19:51:08 -0300

    [Patrick] NA does not test for index data file when TOGGLE_uOPfBeRx_HOME_EDIT is on

--- test/integration/news.js
+++ test/integration/news.js
@@ -487,7 +487,9 @@ describe('REST API:', function() {
               assert.equal(result.status, 'published');
               assert.equal(result.metadata.url, buildNewsHTTPPath(tabloid.metadata.title));
 
-              assert.ok(fs.existsSync(hexoPaths.sourcePath + '/index.md'));
+              if(!process.env.TOGGLE_uOPfBeRx_HOME_EDIT) {
+                assert.ok(fs.existsSync(hexoPaths.sourcePath + '/index.md'));
+              }
 
               var tabloidFileAsFrontMatters = fs.readFileSync(hexoPaths.postsPath + newsYearMonthURL + tabloidId + '.md', 'utf-8');
               var tabloidFileAsObj = grayMatter(tabloidFileAsFrontMatters);
