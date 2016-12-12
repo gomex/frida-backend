a1afbc71d4df0c219190db5a09da4e8f1ebafba6
Michael Feitosa <mkaelzinho@gmail.com>
2016-12-12 15:21:14 -0200

    [kael] 3qjkHBeN Add cover mobile version to presenter

--- test/factories/cover-attributes.js
+++ test/factories/cover-attributes.js
@@ -1,13 +1,20 @@
 var Factory = require('rosie').Factory;
 var faker = require('faker');
 
+var mobile = new Factory()
+  .attr('link', faker.image.imageUrl)
+  .attr('thumbnail', faker.image.imageUrl)
+  .attr('medium', faker.image.imageUrl)
+  .attr('small', faker.image.imageUrl);
+
 var cover = new Factory()
-  .attr('link', function() { return faker.image.imageUrl(); })
-  .attr('thumbnail', function() { return faker.image.imageUrl(); })
-  .attr('medium', function() { return faker.image.imageUrl(); })
-  .attr('small', function() { return faker.image.imageUrl(); })
+  .attr('link', faker.image.imageUrl)
+  .attr('thumbnail', faker.image.imageUrl)
+  .attr('medium', faker.image.imageUrl)
+  .attr('small', faker.image.imageUrl)
   .attr('title', 'The picture\'s title')
   .attr('credits', 'Photographer')
-  .attr('subtitle', 'A beautiful picture');
+  .attr('subtitle', 'A beautiful picture')
+  .attr('mobile', () => mobile.build());
 
 module.exports.cover = cover;
