5190f0f7047b63dcfb029803d2ea956abc7c6c24
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-12-27 16:55:20 -0300

    [Patrick] HHqIAfeh adds migration to replace user in soundcloud's urls

---
+++ migrations/1482853370437-Replaces-soundclouds-user-in-audio-urls.js
@@ -0,0 +1,33 @@
+exports.up = function(next) {
+  var News = this.model('News');
+  News.find({ tags: { $in: ['radioagencia'] } }, (err, allNews) => {
+    if(err) {
+      console.error('Could not find news: ', err);
+      return;
+    }
+
+    allNews.forEach((oneNews) => {
+      oneNews.audio = oneNews.audio.replace('user-146107752', 'radioagenciabdf');
+      oneNews.save((err) => { if(err) console.log('Could not update news id ', oneNews._id, err);});
+    });
+
+    next();
+  });
+};
+
+exports.down = function(next) {
+  var News = this.model('News');
+  News.find({ tags: { $in: ['radioagencia'] } }, (err, allNews) => {
+    if(err) {
+      console.error(err);
+      return;
+    }
+
+    allNews.forEach((oneNews) => {
+      oneNews.audio = oneNews.audio.replace('radioagenciabdf', 'user-146107752');
+      oneNews.save((err) => { if(err) console.log('Could not update news id ', oneNews._id, err);});
+    });
+
+    next();
+  });
+};
