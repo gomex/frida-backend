2a2e9b000cb52170917e89c5cbb19b73d153bf69
Thiago Hersan <thiago.hersan+github@gmail.com>
2017-01-05 13:03:50 -0200

    [thiago] NA replaces lorempixel images used on posts with static images from our flickr

--- test/factories/cover-attributes.js
+++ test/factories/cover-attributes.js
@@ -1,19 +1,18 @@
 var Factory = require('rosie').Factory;
-var faker = require('faker');
 
 var mobile = new Factory()
-  .attr('link', faker.image.imageUrl)
-  .attr('original', faker.image.imageUrl)
-  .attr('thumbnail', faker.image.imageUrl)
-  .attr('medium', faker.image.imageUrl)
-  .attr('small', faker.image.imageUrl);
+  .attr('link', '//farm1.staticflickr.com/479/31746491920_b1f60e17c9_o.jpg')
+  .attr('original', '//farm1.staticflickr.com/479/31746491920_b1f60e17c9_o.jpg')
+  .attr('thumbnail', '//farm1.staticflickr.com/479/31746491920_b1f60e17c9_o.jpg')
+  .attr('medium', '//farm1.staticflickr.com/479/31746491920_b1f60e17c9_o.jpg')
+  .attr('small', '//farm1.staticflickr.com/479/31746491920_b1f60e17c9_o.jpg');
 
 var cover = new Factory()
-  .attr('link', () => faker.image.imageUrl(600,400))
-  .attr('original', () => faker.image.imageUrl(600,400))
-  .attr('thumbnail', () => faker.image.imageUrl(600,400))
-  .attr('medium', () => faker.image.imageUrl(600,400))
-  .attr('small', () => faker.image.imageUrl(600,400))
+  .attr('link', '//farm1.staticflickr.com/328/32082747946_d5bcc2ae95_o.jpg')
+  .attr('original', '//farm1.staticflickr.com/328/32082747946_d5bcc2ae95_o.jpg')
+  .attr('thumbnail', '//farm1.staticflickr.com/328/32082747946_d5bcc2ae95_o.jpg')
+  .attr('medium', '//farm1.staticflickr.com/328/32082747946_d5bcc2ae95_o.jpg')
+  .attr('small', '//farm1.staticflickr.com/328/32082747946_d5bcc2ae95_o.jpg')
   .attr('title', 'The picture\'s title')
   .attr('credits', 'Photographer')
   .attr('subtitle', 'A beautiful picture')
