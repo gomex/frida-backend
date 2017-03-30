4b5d293c9ba18b3faf3d9163fb1110e19afef313
NicholasNeto <nicholasneto@outlook.com>
2017-03-30 11:48:06 -0300

    [Nicholas] BOPiiWgm Searching for news

--- test/unit/lib/services/publisher/archives.js
+++ test/unit/lib/services/publisher/archives.js
@@ -1,9 +1,30 @@
+/*eslint no-undef: "off"*/
 var archives = require('lib/services/publisher/archives.js');
+var hexo = require('lib/services/hexo');
+var postFactory = require('test/factories/post-attributes').post;
+var News = require('lib/models/news');
+
 
 describe('Chronological file', () => {
   var subject = (callback) =>  archives.publish(callback);
 
+  given('post', () => new News(postFactory.build({ published_at: new Date()})));
+
+  beforeEach((done) => { post.save(done); });
+  beforeEach(() => {
+    sandbox.stub(hexo, 'publishArchive').yields();
+  });
+
   it('succeeds', (done) => {
     subject(done);
+  });
+
+  it('publishes with hexo', (done) => {
+    subject((err) => {
+      var args = hexo.publishArchive.args[0];
+      expect(args[0][0].metadata.title).to.deep.equal(post.metadata.title);
+
+      done(err);
+    });
   });
 });
